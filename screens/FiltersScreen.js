07c4cf45cdd3d1d34c72b9ba4b0a6d3ba2ce4cee
iarmankhan <armankhan9244@gmail.com>
2020-01-01 11:54:55 +0530

    Added fonts & Basic screens content

---
+++ screens/FiltersScreen.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const FiltersScreen = props => {
+    return (
+        <View>
+            <Text>CategoriesScreen</Text>
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+});
+
+export default FiltersScreen
