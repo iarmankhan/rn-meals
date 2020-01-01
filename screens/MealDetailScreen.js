ac8967ec325dce9866e29c0959c0ad16f577bded
iarmankhan <armankhan9244@gmail.com>
2020-01-01 16:47:28 +0530

    Added navigation to particular meal and set screen title

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,12 +1,28 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import {View, Text, StyleSheet} from 'react-native'
+
+import {MEALS} from "../data/dummy-data";
+import Meal from "../models/meal";
 
 const MealDetailScreen = props => {
+    const mealId = props.navigation.getParam('mealId');
+    const selectedMeal = MEALS.find(meal => meal.id === mealId);
+
     return (
         <View style={styles.screen}>
             <Text>MealDetailScreen</Text>
         </View>
     )
+};
+
+MealDetailScreen.navigationOptions = navigationData => {
+    const mealId = navigationData.navigation.getParam('mealId');
+
+    const selectedMeal = MEALS.find(meal => meal.id === mealId);
+
+    return {
+        headerTitle: selectedMeal.title
+    }
 };
 
 const styles = StyleSheet.create({
