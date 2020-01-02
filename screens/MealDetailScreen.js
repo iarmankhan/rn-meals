e08d016ec93e22d856be9d10d922473c765c28b6
iarmankhan <armankhan9244@gmail.com>
2020-01-02 12:59:48 +0530

    Added Meal Details Screen contents

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,19 +1,44 @@
 import React from 'react'
-import {View, Text, StyleSheet} from 'react-native'
+import {ScrollView, View, Text, StyleSheet, Image, Dimensions} from 'react-native'
 import {HeaderButtons, Item} from "react-navigation-header-buttons";
 
 import {MEALS} from "../data/dummy-data";
-import Meal from "../models/meal";
+
 import CustomHeaderButton from "../components/HeaderButton";
+import DefaultText from "../components/DefaultText";
+import Colors from "../constants/Colors";
+import {Ionicons} from "@expo/vector-icons";
+
+const ListItem = props => {
+    return (
+        <View style={styles.listItem}>
+            <DefaultText>{props.children}</DefaultText>
+        </View>
+    )
+};
 
 const MealDetailScreen = props => {
     const mealId = props.navigation.getParam('mealId');
     const selectedMeal = MEALS.find(meal => meal.id === mealId);
 
     return (
-        <View style={styles.screen}>
-            <Text>MealDetailScreen</Text>
-        </View>
+        <ScrollView>
+            <Image
+                source={{
+                    uri: selectedMeal.imageUrl
+                }}
+                style={styles.image}
+            />
+            <View style={styles.details}>
+                <DefaultText style={{color: Colors.accentColor}}><Ionicons name="ios-hourglass" size={15} color={Colors.accentColor} /> {selectedMeal.duration}m</DefaultText>
+                <DefaultText style={{color: Colors.accentColor}}><Ionicons name="ios-checkmark-circle-outline" size={15} color={Colors.accentColor} /> {selectedMeal.complexity.toUpperCase()}</DefaultText>
+                <DefaultText style={{color: Colors.accentColor}}><Ionicons name="ios-cash" size={15} color={Colors.accentColor} /> {selectedMeal.affordability.toUpperCase()}</DefaultText>
+            </View>
+            <Text style={styles.title}>Ingredients</Text>
+            {selectedMeal.ingredients.map(ingredient => <ListItem key={ingredient}>{ingredient}</ListItem>)}
+            <Text style={styles.title}>Steps</Text>
+            {selectedMeal.steps.map(step => <ListItem key={step}>{step}</ListItem>)}
+        </ScrollView>
     )
 };
 
@@ -37,10 +62,26 @@ MealDetailScreen.navigationOptions = navigationData => {
 };
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+    image: {
+        width: '100%',
+        height: Dimensions.get('window').height / 4.5
+    },
+    details: {
+        flexDirection: 'row',
+        padding: 15,
+        justifyContent: 'space-around'
+    },
+    title: {
+        fontFamily: 'open-sans-bold',
+        textAlign: 'center',
+        fontSize: 22
+    },
+    listItem: {
+        marginVertical: 10,
+        marginHorizontal: 20,
+        borderColor: '#ccc',
+        borderWidth: 1,
+        padding: 10
     }
 });
 
