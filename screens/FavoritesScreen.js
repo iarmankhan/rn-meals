d6c56136fd41faace3d113db13381699fa95c17f
iarmankhan <armankhan9244@gmail.com>
2020-01-03 10:13:19 +0530

    Displayed fallback message if no favorites are present

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -4,9 +4,19 @@ import MealList from "../components/MealList";
 import { HeaderButtons, Item} from "react-navigation-header-buttons";
 
 import CustomHeaderButton from '../components/HeaderButton'
+import DefaultText from "../components/DefaultText";
+import {View, StyleSheet} from "react-native";
 
 const FavoritesScreen = props => {
     const favMeals = useSelector(state => state.meals.favoriteMeals);
+
+    if(favMeals.length === 0 || !favMeals) {
+        return (
+            <View style={styles.content}>
+                <DefaultText>No favorite meals found. Start adding some!</DefaultText>
+            </View>
+        )
+    }
 
     return <MealList listData={favMeals} navigation={props.navigation} />
 };
@@ -23,5 +33,13 @@ FavoritesScreen.navigationOptions = navData => {
         )
     }
 };
+
+const styles = StyleSheet.create({
+    content: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
 
 export default FavoritesScreen
